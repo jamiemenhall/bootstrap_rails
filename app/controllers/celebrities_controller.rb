ddf439eaddfd99bf1555aa82a3178d3d84d6dc21
Jamie Menhall <jamiemenhall@mac.com>
2016-12-19 15:14:36 -0500

    work with params hash

--- app/controllers/celebrities_controller.rb
+++ app/controllers/celebrities_controller.rb
@@ -2,4 +2,9 @@ class CelebritiesController < ApplicationController
 	def index
 		@celebrities = Celebrity.all
 	end
+	def show
+		puts "we are in the show action"
+		puts "the params are #{params.inspect}"
+		puts "#{params[:yolo]}"
+	end
 end
